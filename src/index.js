068dc3f261e5f95ad9583772538cb1ad42c3e2ca
Hassan-Ahamad <141420993+Hassan-Ahamad@users.noreply.github.com>
2025-06-10 14:05:30 -0700

    add more changes

--- src/index.js
+++ src/index.js
@@ -5,5 +5,14 @@ import { initSocket } from "./utils/socket.io.js";
 await databaseConnection();
 
 // ye section hata diya (no server.listen)
+// app.js ke andar ye add kar do:
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server is live! 🚀",
+    status: "success"
+  });
+});
+
 
 export default app;
