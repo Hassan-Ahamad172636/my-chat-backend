f9bb8007e2fdfa3086ea9a4e1dcd2976ca4a41af
Hassan-Ahamad <141420993+Hassan-Ahamad@users.noreply.github.com>
2025-06-10 14:01:40 -0700

    add some changes in index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
-import http from "http";
 import { app } from "./app.js";
 import { databaseConnection } from "./database/server.js";
 import { initSocket } from "./utils/socket.io.js";
 
-databaseConnection()
-  .then(() => {
-    // const PORT = process.env.PORT;
+await databaseConnection();
 
-    const server = http.createServer(app);
+// ye section hata diya (no server.listen)
 
-    const io = initSocket(server);
-
-    server.listen(port, () => {
-      console.log(
-        `\n==================================\n server is running at port: ${PORT}! \n==================================\n`
-      );
-    });
-  })
-  .catch((err) => {
-    console.log(
-      "\n==================================\n server not start! \n==================================\n"
-    );
-    console.error(err);
-  });
+export default app;
