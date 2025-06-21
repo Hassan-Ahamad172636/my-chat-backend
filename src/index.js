f9ccbb38e2663ba3881ea31cce59485790336065
Hassan Ahmad <hassanahmad172636@gmail.com>
2025-06-20 20:57:40 -0700

    correct

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = (req, res) => {
-  res.status(200).send("pong");
-};
+// src/index.js
+export default function handler(req, res) {
+  res.status(200).send("Hello from ES Module!");
+}
